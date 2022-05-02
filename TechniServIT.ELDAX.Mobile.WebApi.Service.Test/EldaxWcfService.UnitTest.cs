ea1375939ebeb54544d1bc5c28196238bd8771c7
KostasDevelope <ugmcncprogram@gmail.com>
2022-05-02 10:29:57 +0300

    soap request

--- TechniServIT.ELDAX.Mobile.WebApi.Service.Test/EldaxWcfService.UnitTest.cs
+++ TechniServIT.ELDAX.Mobile.WebApi.Service.Test/EldaxWcfService.UnitTest.cs
@@ -8,7 +8,7 @@ namespace TechniServIT.ELDAX.Mobile.WebApi.Service.Test
     public class EldaxWcfServiceUnitTest
     {
         private HttpWebRequest _eldaxService;
-        private string _urlWcf = "https://localhost:8003/ELDAX/Service";
+        private string _urlWcf = "https://localhost:8003/ELDAX/Service.svc/";
 
 
         [SetUp]
@@ -37,23 +37,38 @@ namespace TechniServIT.ELDAX.Mobile.WebApi.Service.Test
         [Test]
         public async Task AuthenticateExTestAsync2()
         {
-            using (var httpResponse = (HttpWebResponse)await _eldaxService.GetResponseAsync())
-            {
-                if (httpResponse.StatusCode == HttpStatusCode.OK)
-                {
-                    using (var objResponseStream = httpResponse.GetResponseStream())
-                    {
-                        using (var objXMLReader = new XmlTextReader(objResponseStream))
-                        {
-                            var xmldoc = new XmlDocument();
-                            xmldoc.Load(objXMLReader);
-                        }
-                    }
-                }
-            }
+            GetXmlDocument();
+
+            //using (var httpResponse = (HttpWebResponse)await _eldaxService.GetResponseAsync())
+            //{
+            //    if (httpResponse.StatusCode == HttpStatusCode.OK)
+            //    {
+            //        using (var objResponseStream = httpResponse.GetResponseStream())
+            //        {
+            //            using (var objXMLReader = new XmlTextReader(objResponseStream))
+            //            {
+            //                var xmldoc = new XmlDocument();
+            //                xmldoc.Load(objXMLReader);
+            //            }
+            //        }
+            //    }
+            //}
 
         }
 
+        private string GetXmlDocument()
+        {
+            var result = string.Empty;
+            var documentXml = new XmlDocument();
+            documentXml.LoadXml(ELDAXServiceRequests.AuthenticateEx);
+            var nsmgr = new XmlNamespaceManager(documentXml.NameTable);
+            nsmgr.AddNamespace("soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
+            nsmgr.AddNamespace("tec", "https://www.techniserv-it.cz");
+            nsmgr.AddNamespace("tec1", "http://schemas.datacontract.org/2004/07/TechniServIT.ELDAX.Service.ProxyClasses");
+            var applicationLogin = documentXml.SelectSingleNode("//tec:AuthenticateEx/tec:ctx/tec1:ApplicationLogin", nsmgr);
+            applicationLogin.InnerText = "sa";
+            return documentXml.InnerXml;
+        }
 
     }
 }
